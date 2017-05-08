79f316995cf809ad203daeb2799692e9f6309211
Marcus Ogden <marcusogden@Marcuss-MBP.PK5001Z>
2017-05-08 08:17:32 -0400

    this is the final one before i go ham

---
+++ js.js
@@ -0,0 +1,6 @@
+function loader(){
+  document.getElementById('faux-load').style.display = 'flex'
+  setTimeout(function(){
+    window.location.href = './Alt Designs/one/index.html'
+  }, 2000);
+}
