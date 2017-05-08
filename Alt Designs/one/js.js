9d343fdd616e05d6e45499540b714a22109b7b1a
Marcus Ogden <marcusogden@Marcuss-MBP.PK5001Z>
2017-05-08 07:40:13 -0400

    pudate

--- Alt Designs/one/js.js
+++ Alt Designs/one/js.js
@@ -31,28 +31,64 @@ function run(){
 function colorPickerSelect(color){
   //left upper arm control
   if(color === 'la-green'){
+    document.getElementById('left-upper-arm-label').style.background = 'green'
+    document.getElementById("left-upper-arm-title").innerHTML = "Green";
+    document.getElementById('upper-right-arm-svg').style.fill = 'green'
+    document.getElementById('upper-right-arm-svg').style.stroke = 'black'
     document.getElementById('color-picker-dorpdown-left-arm').style.display = 'none'
     dropdownCtrl = true
   }
   if(color === 'la-blue'){
+    document.getElementById('left-upper-arm-label').style.background = 'lightblue'
+    document.getElementById("left-upper-arm-title").innerHTML = "Blue";
+    document.getElementById('upper-right-arm-svg').style.fill = 'lightblue'
+    document.getElementById('upper-right-arm-svg').style.stroke = 'black'
     document.getElementById('color-picker-dorpdown-left-arm').style.display = 'none'
     dropdownCtrl = true
   }
   if(color === 'la-red'){
+    document.getElementById('left-upper-arm-label').style.background = 'red'
+    document.getElementById("left-upper-arm-title").innerHTML = "Red";
+    document.getElementById('upper-right-arm-svg').style.fill = 'red'
+    document.getElementById('upper-right-arm-svg').style.stroke = 'black'
     document.getElementById('color-picker-dorpdown-left-arm').style.display = 'none'
     dropdownCtrl = true
   }
   //right upper arm control
   if(color === 'rua-green'){
+    document.getElementById('right-upper-arm-label').style.background = 'Green'
+    document.getElementById("right-upper-arm-title").innerHTML = "Green";
+    document.getElementById('upper-left-arm-svg').style.fill = 'green'
+    document.getElementById('upper-left-arm-svg').style.stroke = 'black'
     document.getElementById('color-picker-dorpdown-right-upper-arm').style.display = 'none'
     dropdownCtrl = true
   }
   if(color === 'rua-blue'){
+    document.getElementById('right-upper-arm-label').style.background = 'lightblue'
+    document.getElementById("right-upper-arm-title").innerHTML = "Blue";
+    document.getElementById('upper-left-arm-svg').style.fill = 'lightblue'
+    document.getElementById('upper-left-arm-svg').style.stroke = 'black'
     document.getElementById('color-picker-dorpdown-right-upper-arm').style.display = 'none'
     dropdownCtrl = true
   }
   if(color === 'rua-red'){
+    document.getElementById('right-upper-arm-label').style.background = 'Red'
+    document.getElementById("right-upper-arm-title").innerHTML = "Red";
+    document.getElementById('upper-left-arm-svg').style.fill = 'red'
+    document.getElementById('upper-left-arm-svg').style.stroke = 'black'
     document.getElementById('color-picker-dorpdown-right-upper-arm').style.display = 'none'
     dropdownCtrl = true
   }
+}
+// Save suit options - make sure you require them to watch the video and add measurements before 
+
+
+
+
+
+//testing
+function testSVG(){
+  alert("Acknowledge to modify object color.");
+  var object = document.getElementById("right-upper-arm-SVG");
+  object.setAttribute("fill", "green");
 }
